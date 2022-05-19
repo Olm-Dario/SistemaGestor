f693f18a4a023673764ecbfa9931ecf82a5f33ee
dezaelDev <emmanuelfernandez.developer@gmail.com>
2022-05-19 00:11:30 -0300

    Listar method for clients - Created

---
+++ CapaNegocio/CapaNegocio_Cliente.cs
@@ -0,0 +1,22 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CapaNegocio_Cliente
+    {
+
+        private CapaDato_Cliente objCD_cliente = new CapaDato_Cliente();
+
+        public List<Cliente> Listar()
+        {
+            return objCD_cliente.Listar();
+        }
+    }
+}
+
