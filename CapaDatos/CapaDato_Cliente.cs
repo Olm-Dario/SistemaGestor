3fa3bbd07ae3e01a0d85ebd786837ac6c1b585dd
dezaelDev <emmanuelfernandez.developer@gmail.com>
2022-05-18 23:03:11 -0300

    Editar method for client - Created

--- CapaDatos/CapaDato_Cliente.cs
+++ CapaDatos/CapaDato_Cliente.cs
@@ -94,6 +94,47 @@ namespace CapaDatos
 
             return idClientegenerado;
         }
+
+        //Metodo para editar clientes en la BD
+        public bool Editar(Cliente obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarCliente", conexion);
+                    cmd.Parameters.AddWithValue("IdCliente", obj.idCliente);
+                    cmd.Parameters.AddWithValue("Documento", obj.documento);
+                    cmd.Parameters.AddWithValue("Nombre", obj.nombre);
+                    cmd.Parameters.AddWithValue("Apellido", obj.apellido);
+                    cmd.Parameters.AddWithValue("Correo", obj.correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.telefono);
+                    cmd.Parameters.AddWithValue("Estado", obj.estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    conexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+
     }
 
    
