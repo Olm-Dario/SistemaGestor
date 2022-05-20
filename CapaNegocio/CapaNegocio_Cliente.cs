dabe3e3e03cfd3a597a200e99108168983d46521
dezaelDev <emmanuelfernandez.developer@gmail.com>
2022-05-20 14:50:55 -0300

    Registrar method for client - Created - Business

--- CapaNegocio/CapaNegocio_Cliente.cs
+++ CapaNegocio/CapaNegocio_Cliente.cs
@@ -11,11 +11,51 @@ namespace CapaNegocio
     public class CapaNegocio_Cliente
     {
 
-        private CapaDato_Cliente objCD_cliente = new CapaDato_Cliente();
+        private readonly CapaDato_Cliente objCD_cliente = new CapaDato_Cliente();
 
         public List<Cliente> Listar()
         {
             return objCD_cliente.Listar();
+        }
+
+        public int Registrar(Cliente obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if(obj.documento == "")
+            {
+                Mensaje += "Es necesario el documento del cliente\n";
+            }
+
+            if(obj.nombre == "")
+            {
+                Mensaje += "Es necesario el nombre del cliente\n";
+            }
+
+            if (obj.apellido == "")
+            {
+                Mensaje += "Es necesario el apellido del cliente\n";
+            }
+
+            if (obj.correo == "")
+            {
+                Mensaje += "Es necesario el correo del cliente\n";
+            }
+
+            if (obj.telefono == "")
+            {
+                Mensaje += "Es necesario el telefono del cliente\n";
+            }
+
+
+            if (Mensaje!= string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objCD_cliente.Registrar(obj, out Mensaje);
+            }
         }
     }
 }
