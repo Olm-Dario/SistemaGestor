867c18bbecb74f7cd09d9c43b6ecbc0faa91db48
Dario Olmedo <dariolmedo2@gmail.com>
2022-05-22 19:49:51 -0300

    Procedimientos en la capa de negocio

--- CapaNegocio/CapaNegocio_Usuario.cs
+++ CapaNegocio/CapaNegocio_Usuario.cs
@@ -16,5 +16,86 @@ namespace CapaNegocio
         {
             return objCD_usuario.listar();
         }
+
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            /*REGLAS DE NEGOCIO*/
+
+            if (obj.documento == "")
+            {
+                Mensaje += "Es necesario el documento del usuario\n";
+            }
+
+            if (obj.nombre == "")
+            {
+                Mensaje += "Es necesario el nombre del usuario\n";
+            }
+
+            if (obj.apellido == "")
+            {
+                Mensaje += "Es necesario el apellido del usuario\n";
+            }
+
+            if (obj.clave == "")
+            {
+                Mensaje += "Es necesario la clave del usuario\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                return objCD_usuario.Registrar(obj, out Mensaje);
+            }
+
+            
+        }
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            /*REGLAS DE NEGOCIO*/
+
+            if (obj.documento == "")
+            {
+                Mensaje += "Es necesario el documento del usuario\n";
+            }
+
+            if (obj.nombre == "")
+            {
+                Mensaje += "Es necesario el nombre del usuario\n";
+            }
+
+            if (obj.apellido == "")
+            {
+                Mensaje += "Es necesario el apellido del usuario\n";
+            }
+
+            if (obj.clave == "")
+            {
+                Mensaje += "Es necesario la clave del usuario\n";
+            }
+
+            if (Mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                return objCD_usuario.Editar(obj, out Mensaje);
+            }
+            
+        }
+
+        public bool Eliminar(Usuario obj, out string Mensaje)
+        {
+            return objCD_usuario.Eliminar(obj, out Mensaje);
+        }
+
     }
 }
