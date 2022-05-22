02cb2ba03aed63d89471560fdd3582d5cb37e05f
Dario Olmedo <dariolmedo2@gmail.com>
2022-05-22 19:33:15 -0300

    Procedimiento de eliminacion de usuario

--- CapaDatos/CapaDato_Usuario.cs
+++ CapaDatos/CapaDato_Usuario.cs
@@ -106,7 +106,7 @@ namespace CapaDatos
 
                     //Obtenemos los valores de los paramatros de salida despues de la ejecucion
                     idUsuarioGenerado = Convert.ToInt32(cmd.Parameters["IdUsuarioResultado"].Value);
-                    Mensaje = cmd.Parameters["IdUsuarioResultado"].Value.ToString();
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
 
                 }
 
@@ -158,7 +158,52 @@ namespace CapaDatos
 
                     //Obtenemos los valores de los paramatros de salida despues de la ejecucion
                     respuesta = Convert.ToBoolean(cmd.Parameters["IdUsuarioResultado"].Value);
-                    Mensaje = cmd.Parameters["IdUsuarioResultado"].Value.ToString();
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return respuesta;
+        }
+
+        public bool Eliminar(Usuario obj, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    //le pasamos el procedimiento almacenado
+                    SqlCommand cmd = new SqlCommand("SP_ELIMINARUSUARIO", conexion);
+
+                    //Le pasamos los parametros de entrada
+                    cmd.Parameters.AddWithValue("IdUsuario", obj.idUsuario);
+
+                    //Le pasamos los parametros de salida
+                    cmd.Parameters.Add("Respuesta", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar).Direction = ParameterDirection.Output;
+
+                    //Le que el tipo de comando es un procedimiento almacenado
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    //Abrimos la cadena de conexion para que se ejecute el comando
+                    conexion.Open();
+
+                    //Ejecutamos el comando
+                    cmd.ExecuteNonQuery();
+
+                    //Obtenemos los valores de los paramatros de salida despues de la ejecucion
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Respuesta"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
 
                 }
 
