ed12310277c3a5dc901fa72c59867df98b5087a5
Dario Olmedo <dariolmedo2@gmail.com>
2022-05-30 10:42:45 -0300

    Class CapaDato_Negocio, metodos ObtenerDatos(), GuardarDatos(), ObtenerLogo(),ActualizarLogo() - Agregado - CapaDato

---
+++ CapaDatos/CapaDato_Negocio.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using CapaEntidad;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CapaDato_Negocio
+    {
+
+        public Negocio ObtenerDatos()
+        {
+            Negocio obj = new Negocio();
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    conexion.Open();
+
+                    string query = "select IdNegocio,Nombre,Direccion from NEGOCIO where IdNegocio = 1";
+                    SqlCommand cmd = new SqlCommand(query,conexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            obj = new Negocio()
+                            {
+                                IdNegocio = Convert.ToInt32(dr["IdNegocio"].ToString()),
+                                Nombre = dr["Nombre"].ToString(),
+                                Direccion = dr["Direccion"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = new Negocio();
+            }
+
+            return obj;
+        }
+
+        public bool GuardarDatos(Negocio obj, out string mensaje)
+        {
+            mensaje = string.Empty;
+            bool respuesta = true;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    conexion.Open();
+
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("update NEGOCIO set Nombre = @nombre,");
+                    query.AppendLine("Direccion = @direccion,");
+                    query.AppendLine("where IdNegocio = 1");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(),conexion);
+                    cmd.Parameters.AddWithValue("@nombre",obj.Nombre);
+                    cmd.Parameters.AddWithValue("@direccion", obj.Direccion);
+                    cmd.CommandType = CommandType.Text;
+
+                    if (cmd.ExecuteNonQuery() < 1)
+                    {
+                        mensaje = "No se pudo guardar los datos";
+                        respuesta = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+                respuesta = false;
+            }
+
+            return respuesta;
+        }
+
+        //Devuelve un array de byte
+        //Si obtiene exitosamente obtenido sera true
+        public byte[] ObtenerLogo(out bool obtenido)
+        {
+            obtenido = true;
+            byte[] LogoBytes = new byte[0];
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    conexion.Open();
+                    string query = "select IdNegocio,Nombre,Direccion from NEGOCIO where IdNegocio = 1";
+                    SqlCommand cmd = new SqlCommand(query, conexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            LogoBytes = (byte[])dr["Logo"];
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                obtenido = false;
+                LogoBytes = new byte[0];
+            }
+
+            return LogoBytes;
+        }
+
+        public bool ActualizarLogo(byte[] image, out string mensaje)
+        {
+            mensaje = string.Empty;
+            bool respuesta = true;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    conexion.Open();
+
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("update NEGOCIO set Logo = @imagen,");
+                    query.AppendLine("where IdNegocio = 1");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), conexion);
+                    cmd.Parameters.AddWithValue("@imagen", image);
+                    cmd.CommandType = CommandType.Text;
+
+                    //verifica si hay filas afectadas
+                    if (cmd.ExecuteNonQuery() < 1)
+                    {
+                        mensaje = "No se pudo actualizar el logo";
+                        respuesta = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+                respuesta = false;
+            }
+
+            return respuesta;
+        }
+
+    }
+}
