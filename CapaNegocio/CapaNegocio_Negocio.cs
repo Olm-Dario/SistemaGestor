4f167eb5c89de08a45735b5a1cdec616c8163d1c
Dario Olmedo <dariolmedo2@gmail.com>
2022-05-30 11:35:12 -0300

    ObtenerDatos() - Agregado - CapaNegocio_Negocio

--- CapaNegocio/CapaNegocio_Negocio.cs
+++ CapaNegocio/CapaNegocio_Negocio.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CapaDatos;
+using CapaEntidad;
+
 namespace CapaNegocio
 {
     class CapaNegocio_Negocio
     {
+        private CapaDato_Negocio objcd_negocio = new CapaDato_Negocio();
+
+        public Negocio ObtenerDatos()
+        {
+            return objcd_negocio.ObtenerDatos();
+        }
     }
 }
