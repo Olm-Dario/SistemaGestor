75fe8f19b6f5e332cc69f21c54a057559010ae6e
Dario Olmedo <dariolmedo2@gmail.com>
2022-05-30 16:01:56 -0300

    class CapaNegocio_Negocio - Modificada - CapaNegocio

--- CapaNegocio/CapaNegocio_Negocio.cs
+++ CapaNegocio/CapaNegocio_Negocio.cs
@@ -9,7 +9,7 @@ using CapaEntidad;
 
 namespace CapaNegocio
 {
-    class CapaNegocio_Negocio
+    public class CapaNegocio_Negocio
     {
         private CapaDato_Negocio objcd_negocio = new CapaDato_Negocio();
 
