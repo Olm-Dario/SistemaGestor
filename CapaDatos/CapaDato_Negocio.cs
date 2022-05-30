8cec4aa23783c0d0510743ae5dbe7a925e71bd72
Dario Olmedo <dariolmedo2@gmail.com>
2022-05-30 16:43:49 -0300

    ActualizarLogo() - Modificado - CapaDato_Negocio

--- CapaDatos/CapaDato_Negocio.cs
+++ CapaDatos/CapaDato_Negocio.cs
@@ -132,7 +132,7 @@ namespace CapaDatos
                     conexion.Open();
 
                     StringBuilder query = new StringBuilder();
-                    query.AppendLine("update NEGOCIO set Logo = @imagen,");
+                    query.AppendLine("update NEGOCIO set Logo = @imagen");
                     query.AppendLine("where IdNegocio = 1");
 
                     SqlCommand cmd = new SqlCommand(query.ToString(), conexion);
