0414f843bd99bddc687b1a7c4b69969fbdaa3a4e
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-06 20:30:18 -0300

    delete17.png - Agregado - Resources

--- CapaPresentacion/Properties/Resources.Designer.cs
+++ CapaPresentacion/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace CapaPresentacion.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap delete17 {
+            get {
+                object obj = ResourceManager.GetObject("delete17", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap delete32 {
             get {
                 object obj = ResourceManager.GetObject("delete32", resourceCulture);
