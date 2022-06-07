c5e0f0fe299e9dfb7baaf84263fcb05317b1dd79
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-07 20:02:17 -0300

    Registrar() - Agregado - CapaDato_Compra

--- CapaDatos/CapaDato_Compra.cs
+++ CapaDatos/CapaDato_Compra.cs
@@ -46,9 +46,12 @@ namespace CapaDatos
 
             return idCorrelativo;
         }
-        /*
+        
         public bool Registrar(Compra obj, DataTable DetalleCompra, out string Mensaje)
         {
+            bool Respuesta = false;
+            Mensaje = string.Empty;
+
             using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
             {
                 try
@@ -56,22 +59,38 @@ namespace CapaDatos
                     //Ejecutamos la sentancia sql a la base
                     SqlCommand cmd = new SqlCommand("sp_RegistrarCompra", conexion);
 
+                    cmd.Parameters.AddWithValue("IdUsuario",obj.oUsuario.idUsuario);
+                    cmd.Parameters.AddWithValue("IdProveedor", obj.oProveedor.idProveedor);
+                    cmd.Parameters.AddWithValue("TipoDocumento",obj.tipoDocumento);
+                    cmd.Parameters.AddWithValue("NumeroDocumento",obj.numeroDocumento);
+                    cmd.Parameters.AddWithValue("MontoTotal",obj.montoTotal);
+                    cmd.Parameters.AddWithValue("DetalleCompra",DetalleCompra);
+                    //Le pasamos los parametros de salida
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+
                     //Le decimos que es un texto ya que "query" es un string
-                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
 
                     //Abrimos la cadena de conexion para que se ejecute el comando
                     conexion.Open();
 
-                    //Tomamos el valor que nos da la consulta
-                    idCorrelativo = Convert.ToInt32(cmd.ExecuteScalar());
+                    //Obtenemos los valores de los paramatros de salida despues de la ejecucion
+                    Respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
                 }
                 catch (Exception ex)
                 {
                     //En caso de error
-                    idCorrelativo = 0;
+                    Respuesta = false;
+                    Mensaje = ex.Message;
                 }
             }
-        }*/
+
+            return Respuesta;
+        }
 
     }
 }
