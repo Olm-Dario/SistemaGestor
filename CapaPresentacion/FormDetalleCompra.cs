48e8ef309304f0a26b14325daa52dcf4458a0d72
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-08 20:50:01 -0300

    btnBuscar_Click() - Agregado - FormDetalleCompra

--- CapaPresentacion/FormDetalleCompra.cs
+++ CapaPresentacion/FormDetalleCompra.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using CapaEntidad;
+using CapaNegocio;
+
 namespace CapaPresentacion
 {
     public partial class FormDetalleCompra : Form
@@ -15,6 +18,31 @@ namespace CapaPresentacion
         public FormDetalleCompra()
         {
             InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Compra oCompra = new CapaNegocio_Compra().ObtenerCompra(textBusqueda.Text);
+
+            if (oCompra.idCompra !=0 )
+            {
+                textNumeroDocumento.Text = oCompra.numeroDocumento;
+
+                textFecha.Text = oCompra.fechaRegistro;
+                textTipoDocumento.Text = oCompra.tipoDocumento;
+                textUsuario.Text = oCompra.oUsuario.apellido + " " + oCompra.oUsuario.nombre;
+                textDocProveedor.Text = oCompra.oProveedor.documento;
+                textNombreProveedor.Text = oCompra.oProveedor.razonSocial;
+
+                dgvData.Rows.Clear();
+                foreach (Detalle_Compra dc in oCompra.oDetalle_Compra)
+                {
+                    dgvData.Rows.Add(new object[] { dc.oProducto.nombre, dc.precioCompra, dc.cantidad, dc.montoTotal });
+                }
+
+                textMontoTotal.Text = oCompra.montoTotal.ToString("0.00");
+
+            }
         }
     }
 }
