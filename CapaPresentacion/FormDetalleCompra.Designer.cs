6fdc2cbb48f4726cf4ae34448ddd4072587f559e
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-08 18:25:45 -0300

    Diseño Visual - Agregado - FormDetalleCompra

--- CapaPresentacion/FormDetalleCompra.Designer.cs
+++ CapaPresentacion/FormDetalleCompra.Designer.cs
@@ -29,12 +29,362 @@ namespace CapaPresentacion
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textNumeroDocumento = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textFecha = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textNombreProveedor = new System.Windows.Forms.TextBox();
+            this.textDocProveedor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBorrar = new FontAwesome.Sharp.IconButton();
+            this.textBusqueda = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new FontAwesome.Sharp.IconButton();
+            this.textTipoDocumento = new System.Windows.Forms.TextBox();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecioCompra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textMontoTotal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnDescargar = new FontAwesome.Sharp.IconButton();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textNumeroDocumento
+            // 
+            this.textNumeroDocumento.Location = new System.Drawing.Point(477, 45);
+            this.textNumeroDocumento.Name = "textNumeroDocumento";
+            this.textNumeroDocumento.Size = new System.Drawing.Size(95, 20);
+            this.textNumeroDocumento.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo Documento";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label10.Location = new System.Drawing.Point(18, 20);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(147, 25);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Detalle Compra";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textTipoDocumento);
+            this.groupBox1.Controls.Add(this.textUsuario);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textFecha);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(706, 68);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informacion Compra";
+            // 
+            // textFecha
+            // 
+            this.textFecha.Location = new System.Drawing.Point(6, 37);
+            this.textFecha.Name = "textFecha";
+            this.textFecha.ReadOnly = true;
+            this.textFecha.Size = new System.Drawing.Size(177, 20);
+            this.textFecha.TabIndex = 2;
+            this.textFecha.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnDescargar);
+            this.panel1.Controls.Add(this.textMontoTotal);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnBorrar);
+            this.panel1.Controls.Add(this.textBusqueda);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.dgvData);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(121, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 496);
+            this.panel1.TabIndex = 1;
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvData.BackgroundColor = System.Drawing.Color.White;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Producto,
+            this.PrecioCompra,
+            this.Cantidad,
+            this.SubTotal});
+            this.dgvData.Location = new System.Drawing.Point(29, 238);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(700, 204);
+            this.dgvData.TabIndex = 26;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textNumeroDocumento);
+            this.groupBox3.Controls.Add(this.textNombreProveedor);
+            this.groupBox3.Controls.Add(this.textDocProveedor);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(23, 147);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(705, 78);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Informacion Proveedor";
+            // 
+            // textNombreProveedor
+            // 
+            this.textNombreProveedor.Location = new System.Drawing.Point(214, 42);
+            this.textNombreProveedor.Name = "textNombreProveedor";
+            this.textNombreProveedor.ReadOnly = true;
+            this.textNombreProveedor.Size = new System.Drawing.Size(170, 20);
+            this.textNombreProveedor.TabIndex = 3;
+            // 
+            // textDocProveedor
+            // 
+            this.textDocProveedor.Location = new System.Drawing.Point(6, 43);
+            this.textDocProveedor.Name = "textDocProveedor";
+            this.textDocProveedor.Size = new System.Drawing.Size(166, 20);
+            this.textDocProveedor.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(211, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Razon Social:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Numero Documento:";
+            // 
+            // textUsuario
+            // 
+            this.textUsuario.Location = new System.Drawing.Point(465, 37);
+            this.textUsuario.Name = "textUsuario";
+            this.textUsuario.ReadOnly = true;
+            this.textUsuario.Size = new System.Drawing.Size(203, 20);
+            this.textUsuario.TabIndex = 9;
+            this.textUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Numero Documento:";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.BackColor = System.Drawing.Color.White;
+            this.btnBorrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBorrar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBorrar.ForeColor = System.Drawing.Color.Black;
+            this.btnBorrar.IconChar = FontAwesome.Sharp.IconChar.Broom;
+            this.btnBorrar.IconColor = System.Drawing.Color.Black;
+            this.btnBorrar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnBorrar.IconSize = 20;
+            this.btnBorrar.Location = new System.Drawing.Point(630, 44);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(98, 23);
+            this.btnBorrar.TabIndex = 33;
+            this.btnBorrar.Text = "Limpiar";
+            this.btnBorrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBorrar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBorrar.UseVisualStyleBackColor = false;
+            // 
+            // textBusqueda
+            // 
+            this.textBusqueda.Location = new System.Drawing.Point(351, 44);
+            this.textBusqueda.Name = "textBusqueda";
+            this.textBusqueda.Size = new System.Drawing.Size(158, 20);
+            this.textBusqueda.TabIndex = 30;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.White;
+            this.btnBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuscar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.ForeColor = System.Drawing.Color.Black;
+            this.btnBuscar.IconChar = FontAwesome.Sharp.IconChar.Search;
+            this.btnBuscar.IconColor = System.Drawing.Color.Black;
+            this.btnBuscar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnBuscar.IconSize = 20;
+            this.btnBuscar.Location = new System.Drawing.Point(526, 44);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(98, 23);
+            this.btnBuscar.TabIndex = 32;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // textTipoDocumento
+            // 
+            this.textTipoDocumento.Location = new System.Drawing.Point(228, 37);
+            this.textTipoDocumento.Name = "textTipoDocumento";
+            this.textTipoDocumento.ReadOnly = true;
+            this.textTipoDocumento.Size = new System.Drawing.Size(199, 20);
+            this.textTipoDocumento.TabIndex = 10;
+            this.textTipoDocumento.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            // 
+            // PrecioCompra
+            // 
+            this.PrecioCompra.HeaderText = "Precio Compra";
+            this.PrecioCompra.Name = "PrecioCompra";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // SubTotal
+            // 
+            this.SubTotal.HeaderText = "Sub Total";
+            this.SubTotal.Name = "SubTotal";
+            // 
+            // textMontoTotal
+            // 
+            this.textMontoTotal.Location = new System.Drawing.Point(102, 458);
+            this.textMontoTotal.Name = "textMontoTotal";
+            this.textMontoTotal.Size = new System.Drawing.Size(166, 20);
+            this.textMontoTotal.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 461);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Monto Total:";
+            // 
+            // btnDescargar
+            // 
+            this.btnDescargar.BackColor = System.Drawing.Color.White;
+            this.btnDescargar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDescargar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnDescargar.ForeColor = System.Drawing.Color.Black;
+            this.btnDescargar.IconChar = FontAwesome.Sharp.IconChar.ExternalLinkAlt;
+            this.btnDescargar.IconColor = System.Drawing.Color.Black;
+            this.btnDescargar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnDescargar.IconSize = 20;
+            this.btnDescargar.Location = new System.Drawing.Point(555, 461);
+            this.btnDescargar.Name = "btnDescargar";
+            this.btnDescargar.Size = new System.Drawing.Size(173, 23);
+            this.btnDescargar.TabIndex = 34;
+            this.btnDescargar.Text = "Descargar PDF";
+            this.btnDescargar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDescargar.UseVisualStyleBackColor = false;
+            // 
+            // FormDetalleCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(991, 521);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormDetalleCompra";
             this.Text = "FormDetalleCompra";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textNumeroDocumento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textNombreProveedor;
+        private System.Windows.Forms.TextBox textDocProveedor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private FontAwesome.Sharp.IconButton btnBorrar;
+        private System.Windows.Forms.TextBox textBusqueda;
+        private FontAwesome.Sharp.IconButton btnBuscar;
+        private System.Windows.Forms.TextBox textTipoDocumento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecioCompra;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubTotal;
+        private FontAwesome.Sharp.IconButton btnDescargar;
+        private System.Windows.Forms.TextBox textMontoTotal;
+        private System.Windows.Forms.Label label7;
     }
 }
