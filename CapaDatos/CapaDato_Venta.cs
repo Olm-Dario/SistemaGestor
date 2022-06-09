4a70810dea49bfb94ac8616496f335099717f3b7
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-09 18:01:08 -0300

    CapaDato_Venta - Agregado - CapaDato

---
+++ CapaDatos/CapaDato_Venta.cs
@@ -0,0 +1,132 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CapaDato_Venta
+    {
+        public int ObtenerCorrelativo()
+        {
+            int idcorrelativo = 0;
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select count(*) + 1 from VENTA");
+                    SqlCommand cmd = new SqlCommand(query.ToString(), conexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    conexion.Open();
+
+                    idcorrelativo = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+                catch (Exception ex)
+                {
+                    idcorrelativo = 0;
+                }
+            }
+            return idcorrelativo;
+        }
+
+        public bool RestarStock(int idproducto, int cantidad)
+        {
+            bool respuesta = true;
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("update producto set stock - @cantidad where idproducto = @idproducto");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), conexion);
+                    cmd.Parameters.AddWithValue("@cantidad", cantidad);
+                    cmd.Parameters.AddWithValue("@idproducto", idproducto);
+                    cmd.CommandType = CommandType.Text;
+                    conexion.Open();
+
+                    respuesta = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+            }
+            return respuesta;
+        }
+
+        public bool SumarStock(int idproducto, int cantidad)
+        {
+            bool respuesta = true;
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("update producto set stock + @cantidad where idproducto = @idproducto");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), conexion);
+                    cmd.Parameters.AddWithValue("@cantidad", cantidad);
+                    cmd.Parameters.AddWithValue("@idproducto", idproducto);
+                    cmd.CommandType = CommandType.Text;
+                    conexion.Open();
+
+                    respuesta = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+                catch (Exception ex)
+                {
+                    respuesta = false;
+                }
+            }
+            return respuesta;
+        }
+
+        public bool Registrar(Venta obj, DataTable DetalleVenta, out string Mensaje)
+        {
+            bool Respuesta = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("usp_RegistrarVenta", conexion);
+                    cmd.Parameters.AddWithValue("IdUsuario", obj.oUsuario.idUsuario);
+                    cmd.Parameters.AddWithValue("TipoDocumento", obj.tipoDocumento);
+                    cmd.Parameters.AddWithValue("NumeroDocumento", obj.numeroDocumento);
+                    cmd.Parameters.AddWithValue("DocumentoCliente", obj.documentoCliente);
+                    cmd.Parameters.AddWithValue("NombreCliente", obj.nombreCliente);
+                    cmd.Parameters.AddWithValue("MontoPago", obj.montoPago);
+                    cmd.Parameters.AddWithValue("MontoCambio", obj.montoCambio);
+                    cmd.Parameters.AddWithValue("MontoTotal", obj.montoTotal);
+                    cmd.Parameters.AddWithValue("DetalleVenta", DetalleVenta);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conexion.Open();
+                    cmd.ExecuteNonQuery();
+
+                    Respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Respuesta = false;
+                Mensaje = ex.Message;
+            }
+
+            return Respuesta;
+        }
+    }
+}
