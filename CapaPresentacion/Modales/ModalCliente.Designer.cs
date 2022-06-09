ba6737a0fbe9f09ac89e4b8cdcb7e54a60c9d7d6
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-08 23:50:18 -0300

    ModalCliente_Load() - Agregado - ModalCliente

---
+++ CapaPresentacion/Modales/ModalCliente.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace CapaPresentacion.Modales
+{
+    partial class ModalCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLimpiarBusqueda = new FontAwesome.Sharp.IconButton();
+            this.textBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new FontAwesome.Sharp.IconButton();
+            this.cboBuscar = new System.Windows.Forms.ComboBox();
+            this.Documento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label10.Location = new System.Drawing.Point(16, 14);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(156, 25);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "Lista de Clientes";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(2);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Documento,
+            this.Apellido,
+            this.Nombre});
+            this.dgvData.Location = new System.Drawing.Point(21, 104);
+            this.dgvData.MultiSelect = false;
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.Size = new System.Drawing.Size(496, 286);
+            this.dgvData.TabIndex = 29;
+            this.dgvData.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellDoubleClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Buscar Por:";
+            // 
+            // btnLimpiarBusqueda
+            // 
+            this.btnLimpiarBusqueda.BackColor = System.Drawing.Color.White;
+            this.btnLimpiarBusqueda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiarBusqueda.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnLimpiarBusqueda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiarBusqueda.ForeColor = System.Drawing.Color.White;
+            this.btnLimpiarBusqueda.IconChar = FontAwesome.Sharp.IconChar.Broom;
+            this.btnLimpiarBusqueda.IconColor = System.Drawing.Color.Black;
+            this.btnLimpiarBusqueda.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnLimpiarBusqueda.IconSize = 25;
+            this.btnLimpiarBusqueda.Location = new System.Drawing.Point(481, 67);
+            this.btnLimpiarBusqueda.Name = "btnLimpiarBusqueda";
+            this.btnLimpiarBusqueda.Size = new System.Drawing.Size(36, 23);
+            this.btnLimpiarBusqueda.TabIndex = 28;
+            this.btnLimpiarBusqueda.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpiarBusqueda.UseVisualStyleBackColor = false;
+            // 
+            // textBuscar
+            // 
+            this.textBuscar.Location = new System.Drawing.Point(271, 67);
+            this.textBuscar.Name = "textBuscar";
+            this.textBuscar.Size = new System.Drawing.Size(158, 20);
+            this.textBuscar.TabIndex = 25;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.White;
+            this.btnBuscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBuscar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.IconChar = FontAwesome.Sharp.IconChar.Searchengin;
+            this.btnBuscar.IconColor = System.Drawing.Color.Black;
+            this.btnBuscar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnBuscar.IconSize = 25;
+            this.btnBuscar.Location = new System.Drawing.Point(439, 67);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(36, 23);
+            this.btnBuscar.TabIndex = 27;
+            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // cboBuscar
+            // 
+            this.cboBuscar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBuscar.FormattingEnabled = true;
+            this.cboBuscar.Location = new System.Drawing.Point(102, 67);
+            this.cboBuscar.Name = "cboBuscar";
+            this.cboBuscar.Size = new System.Drawing.Size(158, 21);
+            this.cboBuscar.TabIndex = 26;
+            // 
+            // Documento
+            // 
+            this.Documento.HeaderText = "N° Documento";
+            this.Documento.Name = "Documento";
+            this.Documento.ReadOnly = true;
+            this.Documento.Width = 150;
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            this.Apellido.ReadOnly = true;
+            this.Apellido.Width = 150;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 150;
+            // 
+            // ModalCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 399);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnLimpiarBusqueda);
+            this.Controls.Add(this.textBuscar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cboBuscar);
+            this.Name = "ModalCliente";
+            this.Text = "ModalCliente";
+            this.Load += new System.EventHandler(this.ModalCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label label5;
+        private FontAwesome.Sharp.IconButton btnLimpiarBusqueda;
+        private System.Windows.Forms.TextBox textBuscar;
+        private FontAwesome.Sharp.IconButton btnBuscar;
+        private System.Windows.Forms.ComboBox cboBuscar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Documento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+    }
+}
---
+++ CapaPresentacion/Modales/ModalCliente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+using CapaNegocio;
+using CapaPresentacion.Utilidades;
+
+
+namespace CapaPresentacion.Modales
+{
+    public partial class ModalCliente : Form
+    {
+        public Cliente _Cliente { get; set; }
+
+        public ModalCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void ModalCliente_Load(object sender, EventArgs e)
+        {
+            //CARGA DEL ComboBusqueda
+            foreach (DataGridViewColumn column in dgvData.Columns)
+            {
+                cboBuscar.Items.Add(new OpcionCombo() { Valor = column.Name, Texto = column.HeaderText });
+            }
+            cboBuscar.DisplayMember = "Texto";
+            cboBuscar.ValueMember = "Valor";
+            cboBuscar.SelectedIndex = 0;
+
+
+            List<Cliente> listaCliente = new CapaNegocio_Cliente().Listar();
+
+            foreach (Cliente item in listaCliente)
+            {
+                if(item.estado)
+                    dgvData.Rows.Add(new object[]{ item.documento, item.apellido, item.nombre }); 
+            }
+        }
+
+        //private void dgvData_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    //Tomamos indice de fila y columna
+        //    int iRow = e.RowIndex;
+        //    int iColum = e.ColumnIndex;
+
+        //    //Verificamos si existe 
+        //    if (iRow >= 0 && iColum >= 0)
+        //    {
+        //        _Cliente = new Cliente()
+        //        {
+        //            documento = dgvData.Rows[iRow].Cells["Documento"].Value.ToString(),
+        //            apellido = dgvData.Rows[iRow].Cells["Apellido"].Value.ToString(),
+        //            nombre = dgvData.Rows[iRow].Cells["Nombre"].Value.ToString() 
+        //        };
+
+        //        this.DialogResult = DialogResult.OK;
+        //        this.Close();
+        //    }
+        //}
+    }
+}
