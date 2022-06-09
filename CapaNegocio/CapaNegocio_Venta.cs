6d7480fb2da2d7f0b58a4903fc21978e3000a0c4
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-09 18:01:37 -0300

    CapaNegocio_Venta - Agregado - CapaDato

---
+++ CapaNegocio/CapaNegocio_Venta.cs
@@ -0,0 +1,36 @@
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class CapaNegocio_Venta
+    {
+        private CapaDato_Venta objcd_venta = new CapaDato_Venta();
+
+        public int ObtenerCorrelativo()
+        {
+            return objcd_venta.ObtenerCorrelativo();
+        }
+
+        public bool Registrar(Venta obj, DataTable DetalleVenta, out string Mensaje)
+        {
+            return objcd_venta.Registrar(obj, DetalleVenta, out Mensaje);
+        }
+
+        public bool RestarStock(int idproducto, int cantidad)
+        {
+            return objcd_venta.RestarStock(idproducto, cantidad);
+        }
+
+        public bool SumarStock(int idproducto, int cantidad)
+        {
+            return objcd_venta.SumarStock(idproducto, cantidad);
+        }
+    }
+}
