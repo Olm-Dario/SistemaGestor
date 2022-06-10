ccca8f973b243c6bccf808f1c2f81bfc3a18db0c
Dario Olmedo <dariolmedo2@gmail.com>
2022-06-10 17:57:31 -0300

    CapaNegocio_Reporte - Agregado - CapaNegocio

---
+++ CapaNegocio/CapaNegocio_Reporte.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CapaNegocio_Reporte
+    {
+        private CapaDato_Reporte objcd_reporte = new CapaDato_Reporte();
+
+        public List<ReporteCompra> Compra(string fechainicio, string fechafin, int idproveedor)
+        {
+            return objcd_reporte.Compra(fechainicio, fechafin, idproveedor);
+        }
+
+        public List<ReporteVenta> Venta(string fechainicio, string fechafin)
+        {
+            return objcd_reporte.Venta(fechainicio, fechafin);
+        }
+    }
+}
